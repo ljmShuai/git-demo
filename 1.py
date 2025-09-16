c6c9b524354059d05cbb0dd3e4009a8a7527c412
李佳明 <ljm1101525584@163.com>
2025-09-16 23:44:03 +0800

    first save

---
+++ 1.py
@@ -0,0 +1,20 @@
+import random
+lst = [random.randint(1,100) for _ in range(10) ] 
+print(lst)
+
+lst = [i for i in range(10) if i%2==0]
+print(lst)
+
+lst1 = [
+    ['城市','环比', '同比'],
+    ['北京', 102, 103],
+    ['上海', 101, 102],
+    ['深圳',100,39]
+]
+for row in lst1:
+    for item in row:
+        print(item, end='\t')
+    print()
+
+lst2 = [ [j for j in range(4)] for i in range(4)]
+print(lst2)
